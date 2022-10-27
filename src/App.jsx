f3b0ed80ff19e4909345923b490dc7faa97860a9
nikoladj99 <nikoladj@elfak.rs>
2022-10-27 16:39:58 +0200

    proba za remote enrty

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Component2 from "./component2";
-//import Component1 from "app1/Component1";
+import Component1 from "app1/Component1";
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Component1></Component1>
     <Component2></Component2>
   </div>
 );
