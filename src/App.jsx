898d126d0a4f452bc747577af511e802bccbba16
nikoladj99 <nikoladj@elfak.rs>
2022-10-27 16:33:39 +0200

    remotes removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Component2 from "./component2";
-import Component1 from "app1/Component1";
+//import Component1 from "app1/Component1";
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <Component2></Component2>
-    <Component1></Component1>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
